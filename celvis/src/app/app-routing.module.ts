087a5a29747b7c7815782fe3dd6e8701065356aa
Niklas <niklas.buechel@rwth-aachen.de>
2022-04-28 02:38:21 +0200

    Created Angular Project

---
+++ celvis/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./main/main.component";
+
+const routes: Routes = [
+  {
+    path: "main",
+    component: MainComponent,
+    pathMatch: "full",
+  },
+  {
+    path: "",
+    redirectTo: "/main",
+    pathMatch: "full",
+  },
+  {
+    path: "**",
+    redirectTo: "/main",
+    pathMatch: "full",
+  }]
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ celvis/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'celvis';
+
+  // Here nothing logical should be done, only in separate components (for example "main")
+}
