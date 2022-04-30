cda243f5a4e142dfd4dad488c87f5adedfc8b74f
Tristan Höfer <tristan_hoefer@web.de>
2022-04-30 17:17:06 +0200

    PieChart added

---
+++ celvis/src/app/piechart/piechart.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+// CommonJS
+// @ts-ignore
+var Plotly = require('plotly.js-dist')
+
+@Component({
+  selector: 'app-piechart',
+  templateUrl: './piechart.component.html',
+  styleUrls: ['./piechart.component.scss']
+})
+export class PiechartComponent implements OnInit {
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+    this.updatePieChart()
+  }
+
+  updatePieChart() {
+
+    const data  = [{
+      values: [19, 26, 55],
+      labels: ['Residential', 'Non-Residential', 'Utility'],
+      type: 'pie'
+    }];
+
+    const layout = {
+      height: 400,
+      width: 500
+    };
+
+    const config = {responsive: true}
+
+
+    Plotly.newPlot('myDiv1', data, layout, config);
+  }
+}
